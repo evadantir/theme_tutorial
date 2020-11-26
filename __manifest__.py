7eb35ba7d6d253a88697fe5854341e2cf5b9594a
Eva Danti Rahmanita <evadantir@yahoo.com>
2020-11-26 15:09:48 +0700

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+  'name':'Tutorial theme',
+  'description': 'A description for your theme.',
+  'version':'1.0',
+  'author':'Eva',
+
+ 'data': [ 
+            'views/layout.xml',
+            'views/pages.xml',
+            'views/assets.xml',
+            'views/snippets_test.xml'   
+         ],
+  'category': 'Theme/Creative',
+  'depends': ['base','website', 'website_sale','sale_management'],
+  'installable': True
+}
